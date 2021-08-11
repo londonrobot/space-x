aacad37a1eef353f316c411e0753de370a1f4741
londonrobot <utoik@ya.ru>
2021-08-11 18:17:50 +0500

    Published

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var instance = new vidbg(".video", {
+    mp4: "video/world.mp4",
+    webm: "video/world.webm",
+    poster: "video/poster.jpg",
+    overlay: false,
+    overlayColor: "#000",
+    overlayAlpha: 0.3,
+});
+
+var rellax = new Rellax(".rocket");
+
